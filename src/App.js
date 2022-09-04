ba871f178ef78f3d9bc428ddbe227bac7ab40ec5
Rahul Noronha <rahulnor@gmail.com>
2022-09-04 18:19:18 +0530

    updated files
    Changed files of r-place project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Canvas from "./components/Canvas";
 
 function App() {
   return (
-    <div className="App">
+    <div className=" App">
+      <Header/>
+      <BrowserRouter>
+      <Routes>
       
-      <div className='bg-red-400 text-white h-96 w-screen'>Hello</div>
+      <Route exact path="/" element={<Login />} />
+      <Route exact path="/Canvas" element={<Canvas/>}/>
+      </Routes>
+      </BrowserRouter>
+      <Footer/>
+      
     </div>
   );
 }
